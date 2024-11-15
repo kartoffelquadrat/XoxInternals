dc98ee10e16566de3e3b5bcc4b7a93d79d7dc2ff
Maximilian Schiedermeier <schiedermeier.maximilian@uqam.ca>
2024-11-15 09:01:38 -0500

    fixed parts of javadoc location

--- src/main/java/eu/kartoffelquadrat/xoxinternals/controller/XoxManager.java
+++ src/main/java/eu/kartoffelquadrat/xoxinternals/controller/XoxManager.java
@@ -3,24 +3,74 @@ package eu.kartoffelquadrat.xoxinternals.controller;
 import eu.kartoffelquadrat.xoxinternals.model.BoardReadOnly;
 import eu.kartoffelquadrat.xoxinternals.model.Player;
 import eu.kartoffelquadrat.xoxinternals.model.XoxInitSettings;
-
 import java.util.Collection;
 
 public interface XoxManager {
+  /**
+   * Getter for all currently instantiated xox games.
+   *
+   * @return collection of longs serving as game ids.
+   */
+  Collection<Long> getGames();
 
-    Collection<Long> getGames();
+  /**
+   * Removes an existing game, no matter the current state.
+   *
+   * @param gameId as long specifying id of game to remove.
+   */
+  void removeGame(long gameId);
 
-    void removeGame(long gameId);
+  /**
+   * Creates a new game entity with the parameters specified as payload. (players, preferredColours). The game new
+   * game id is created randomly.
+   *
+   * @param initSettings as a settings bundle specifying details for the new game. (Player names, player colours,
+   *                     creator)
+   */
+  long addGame(XoxInitSettings initSettings);
 
-    long addGame(XoxInitSettings initSettings);
+  /**
+   * Getter for the current game board state. Return null if no game is currently initialized.
+   *
+   * @return immutable snapshot of current board.
+   */
+  BoardReadOnly getBoard(long gameId);
 
-    BoardReadOnly getBoard(long gameId);
+  /**
+   * Getter for static player objects (names, preferred colours) of the participants of the game instance referenced
+   * by the provided game-id.
+   *
+   * @param gameId as the identifier of the game to inspect, provided as long.
+   * @return immutably deep copy of players participating in game and their attributes (name, preferred colour). If no
+   * such game is initialized, null is returned.
+   */
+  Player[] getPlayers(long gameId);
 
-    Player[] getPlayers(long gameId);
+  /**
+   * Method to look up possible actions for a given player.
+   *
+   * @param player as the player requesting a set of available actions in a running. Will return an empty collection
+   *               if the player is not recognized.
+   * @return A map, indexing actions by the MD5 representation of their json string serialization. The index serves as
+   * key for later re-identification if an actions is selected. Returns null if no such game is currently initialized.
+   */
+  XoxClaimFieldAction[] getActions(long gameId, String player);
 
-    XoxClaimFieldAction[] getActions(long gameId, String player);
+  /**
+   * Blackboard-style way to allow a client select a specific operation. The operation is identified by the MD5
+   * representation of it's JSON serialization.
+   *
+   * @param player      as the player requesting to play an action
+   * @param actionIndex as the index of the selected action in the original actions array
+   */
+  void performAction(long gameId, String player, int actionIndex);
 
-    void performAction(long gameId, String player, int actionIndex);
-
-    Ranking getRanking(long gameId);
+  /**
+   * Returns current player scores as a serialized ranking object. The ranking object also tells if the game has
+   * already ended.
+   *
+   * @param gameId as the identifier of the game to inspect, provided as long.
+   * @return a ranking bundle object with details on the players and their scores.
+   */
+  Ranking getRanking(long gameId);
 }
--- src/main/java/eu/kartoffelquadrat/xoxinternals/model/BoardReadOnly.java
+++ src/main/java/eu/kartoffelquadrat/xoxinternals/model/BoardReadOnly.java
@@ -6,13 +6,33 @@ package eu.kartoffelquadrat.xoxinternals.model;
  * @author Maximilian Schiedermeier
  */
 public interface BoardReadOnly {
+  /**
+   * Determines whether the play area is entirely empty or not.
+   *
+   * @return boolean indicating if play area is empty.
+   */
+  boolean isEmpty();
 
-    boolean isEmpty();
+  /**
+   * Iterates of a board and returns true if all cells are populated.
+   *
+   * @return boolean telling whether the xox is full.
+   */
+  boolean isFull();
 
-    boolean isFull();
+  /**
+   * Tells whether a given position in x any is currently vacant.
+   *
+   * @param xPos as the horizontal position to test.
+   * @param yPos as the horizontal position to test.
+   * @return vacant status of specified cell.
+   */
+  boolean isFree(int xPos, int yPos);
 
-    boolean isFree(int xPos, int yPos);
-
-    int[][] getCells();
-
+  /**
+   * Returns 2D array representation of xox board, indicating if individual cells are empty, or occupied by a player.
+   *
+   * @return int[][] where first dimension is x, second y.
+   */
+  int[][] getCells();
 }
